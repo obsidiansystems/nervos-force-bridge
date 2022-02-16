7d40dee95bb175d124125b082bde62d9bc15eb89
Skylar-Ray O'Quinn <soraskymizu@gmail.com>
2022-02-16 19:20:38 +0000

    Run ckb dev node in backend

--- backend/src/CKB.hs
+++ backend/src/CKB.hs
@@ -16,6 +16,10 @@ import System.Directory
 import Data.Bool
 import Data.List (intercalate)
 
+import Toml.Codec ( TomlCodec
+                  , HasCodec
+                  , genericCodec
+                  )
 import qualified Toml.Codec as Toml
 
 import GHC.IO.Handle
@@ -29,8 +33,144 @@ import Data.Attoparsec.Text as A
 
 data CkbConfig =
   CkbConfig { data_dir :: T.Text
+            , chain :: ChainSpec
+            , logger :: Logger
+            , sentry :: Sentry
+            , db :: Db
+            , network :: Network
+            , rpc :: RPC
+            , tx_pool :: TxPool
+            , store :: Store
+            , block_assembler :: Maybe BlockAssembler
             }
-  deriving (Generic)
+  deriving (Generic, Show)
+
+newtype SpecFile =
+  SpecFile { file :: T.Text
+           }
+  deriving (Generic, Show)
+
+data ChainSpec =
+  ChainSpec { spec :: SpecFile
+            }
+  deriving (Generic, Show)
+
+data Logger =
+  Logger { filter :: T.Text -- TODO(skylar): This should be an enum probably
+         , color :: Bool
+         , log_to_file :: Bool
+         , log_to_stdout :: Bool
+         }
+  deriving (Generic, Show)
+
+data Sentry =
+  Sentry { dsn :: T.Text
+         }
+  deriving (Generic, Show)
+
+data Db =
+  Db { cache_size :: Integer
+     }
+  deriving (Generic, Show)
+
+data Network =
+  Network { listen_addresses :: [T.Text]
+          , bootnodes :: [T.Text]
+          , max_peers :: Int
+          , max_outbound_peers :: Int
+          , ping_interval_secs :: Int
+          , ping_timeout_secs :: Int
+          , connect_outbound_interval_secs :: Int
+          , upnp :: Bool
+          , discovery_local_address :: Bool
+          , bootnode_mode :: Bool
+          }
+  deriving (Generic, Show)
+
+data RPC =
+  RPC { listen_address :: T.Text
+      , max_request_body_size :: Int
+      , modules :: [T.Text]
+      , reject_ill_transactions :: Bool
+      , enable_deprecated_rpc :: Bool
+      }
+  deriving (Generic, Show)
+
+data TxPool =
+  TxPool { max_mem_size :: Int
+         , max_cycles :: Int
+         , max_verify_cache_size :: Int
+         , max_conflict_cache_size :: Int
+         , max_committed_txs_hash_cache_size :: Int
+         , min_fee_rate :: Int
+         , max_tx_verify_cycles :: Int
+         , max_ancestors_count :: Int
+         }
+  deriving (Generic, Show)
+
+data Store =
+  Store { header_cache_size :: Int
+        , cell_data_cache_size :: Int
+        , block_proposals_cache_size :: Int
+        , block_tx_hashes_cache_size :: Int
+        , block_uncles_cache_size :: Int
+        , cellbase_cache_size :: Int
+        }
+  deriving (Generic, Show)
+
+data BlockAssembler = BlockAssembler
+  { code_hash :: T.Text
+  , args :: T.Text
+  , hash_type :: T.Text
+  , message :: T.Text
+  }
+  deriving (Generic, Show)
+
+mkBlockAssembler :: Account -> BlockAssembler
+mkBlockAssembler a =
+  BlockAssembler
+  magicHash
+  (lock_hash a)
+  alwaysHashType
+  -- TODO(skylar): What is message for?
+  "0x"
+  where
+    magicHash = "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8"
+    alwaysHashType = "type"
+
+ckbConfigCodec :: TomlCodec CkbConfig
+ckbConfigCodec = genericCodec
+
+-- TODO(skylar): This should really be an inline table, but inline tables are just alternative syntax
+instance HasCodec ChainSpec where
+  hasCodec = Toml.table genericCodec
+
+instance HasCodec TxPool where
+  hasCodec = Toml.table genericCodec
+
+instance HasCodec Store where
+  hasCodec = Toml.table genericCodec
+
+instance HasCodec RPC where
+  hasCodec = Toml.table genericCodec
+
+instance HasCodec BlockAssembler where
+  hasCodec = Toml.table genericCodec
+
+instance HasCodec SpecFile where
+  hasCodec = Toml.table genericCodec
+
+instance HasCodec Logger where
+  hasCodec = Toml.table genericCodec
+
+instance HasCodec Sentry where
+  hasCodec = Toml.table genericCodec
+
+instance HasCodec Db where
+  hasCodec = Toml.table genericCodec
+
+instance HasCodec Network where
+  hasCodec = Toml.table genericCodec
 
 data Account =
   Account { mainnet_address :: Address
@@ -65,9 +205,7 @@ account = do
     testnet = addr "testnet"
 
 -- We pipe in a temporary file as Stdin to be able to provide a password to the create account utility
-
-
-{-createNewAccount :: MonadIO m => m Account
+createNewAccount :: MonadIO m => m Account
 createNewAccount = liftIO $ do
   d <- readProcess ckbCliPath ["account", "new"] input
   let Done _ a = parse account . T.pack $ d
@@ -75,40 +213,12 @@ createNewAccount = liftIO $ do
   where
     pass = "hello"
     input = intercalate "\n" [pass, pass]
--}
-
-  {-liftIO $ withTempFile "." "password" $ \_ handle -> do
-  hPutStr handle "hello"
-  hPutStr handle "hello"
-  (_, Just stdout, _, _) <- createProcess $ cp handle
-  hGetContents stdout >>= putStrLn
-  pure undefined
-  where
-    -- TODO(skylar): Use proc or something here
-    cp h =
-      CreateProcess
-      (ShellCommand $ intercalate " " [ckbCliPath, "account", "new"])
-      Nothing
-      Nothing
-      (UseHandle h)
-      Inherit
-      Inherit
-      False
-      False
-      False
-      False
-      False
-      False
-      Nothing
-      Nothing
-      False
 
 ckbPath :: FilePath
 ckbPath = $(staticWhich "ckb")
 
 ckbCliPath :: FilePath
 ckbCliPath = $(staticWhich "ckb-cli")
-
 
 -- TODO(skylar): This fails if certain files already exist
 initDevChain :: (MonadIO m) => FilePath -> Bool -> m ()
@@ -117,9 +227,31 @@ initDevChain path force = liftIO $ do
   -- Just pass <- runConfigsT cfgs $ getConfig "backend/password"
   -- print pass
   _ <- createProcess cp
-  createNewAccount
+  account <- createNewAccount
+  r <- Toml.decodeFile ckbConfigCodec "ckb/ckb.toml"
+  Toml.encodeToFile ckbConfigCodec "ckb/ckb.toml" $ r { block_assembler = Just $ mkBlockAssembler account }
+  _ <- createProcess cp'
   pure ()
   where
+    -- TODO(skylar): This can just be a shell or proc instead of doing this by hand
+    cp' =
+      CreateProcess
+      (ShellCommand $ intercalate " " [ckbPath, "run"])
+      (Just path)
+      Nothing
+      Inherit
+      Inherit
+      Inherit
+      False
+      False
+      False
+      False
+      False
+      False
+      Nothing
+      Nothing
+      False
+
     -- TODO(skylar): This can just be a shell or proc instead of doing this by hand
     cp =
       CreateProcess
@@ -138,10 +270,10 @@ initDevChain path force = liftIO $ do
       Nothing
       Nothing
       False
--}
+
 runDevelopmentChain :: MonadIO m => FilePath -> m ()
 runDevelopmentChain directory = liftIO $ do
-  -- createDirectoryIfMissing False directory
+  createDirectoryIfMissing False directory
   -- TODO(skylar): Can we just detect if this should be forced, and avoid creating this everytime?
-  -- initDevChain directory True
+  initDevChain directory True
   pure ()
--- default.nix
+++ default.nix
@@ -28,5 +28,17 @@ in project ./. ({ pkgs, ... }: {
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
 
-  tools = _: [ pkgs.git ckb ckb-cli pkgs.coreutils ];
+  overrides = self: super: {
+    # canonical-json =
+    # nothunks = self.callHackageDirect {
+    # pkg = "nothunks";
+    # ver = "0.1.3";
+    # sha256 = "1zv28np3k3hg378vqm89v802xr0g8cwk7gy3mr77xrzy5jbgpa39";
+    # /a};
+    # cardano-prelude-test = self.callCabal2nix "cardano-prelude-test"  (sources.cardano-prelude + "/cardano-prelude-test") {};
+    # cardano-prelude = self.callCabal2nix "cardano-prelude"  (sources.cardano-prelude + "/cardano-prelude") {};
+    # cardano-binary = self.callCabal2nix "cardano-binary" (sources.cardano-base + "/binary") {};
+  };
+
+  tools = _: [ ckb ckb-cli pkgs.coreutils ];
 })
